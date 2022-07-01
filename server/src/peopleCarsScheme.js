740b07b3e789ebcc53af03806e55de8493988c38
Jaspreet Singh Bhatti <ijaspreetbhatti@outlook.com>
2022-06-30 23:53:30 -0700

    Updates Static data file

--- server/src/peopleCarsScheme.js
+++ server/src/peopleCarsScheme.js
@@ -1,4 +1,4 @@
-const people = [
+export const people = [
   {
     id: '1',
     firstName: 'Bill',
@@ -16,7 +16,7 @@ const people = [
   }
 ]
 
-const cars = [
+export const cars = [
   {
     id: '1',
     year: '2019',
